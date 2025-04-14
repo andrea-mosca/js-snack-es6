381609b2e7e4fd96d51d3add79907a63f838a231
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-14 14:38:02 +0200

    snack 2: array & random number function

--- js/main.js
+++ js/main.js
@@ -29,3 +29,18 @@ for(let i = 1; i < bikes.length; i++){
     }
 }
 console.log(bikeMinWeight);
+
+// * SNACK 2
+const footballTeams = [
+    {name:"squadra 1", points: 0, sufferedFouls: 0},
+    {name:"squadra 1", points: 0, sufferedFouls: 0},
+    {name:"squadra 1", points: 0, sufferedFouls: 0},
+    {name:"squadra 1", points: 0, sufferedFouls: 0},
+    {name:"squadra 1", points: 0, sufferedFouls: 0}
+];
+
+// funzione che genera numeri casuali
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
